375f38fb9ef7e0275f1bc3485a9362b8ab9c1db0
doellamoella <92926184+doellamoella@users.noreply.github.com>
2022-02-26 11:48:58 +0530

    Create alive.py

---
+++ alive.py
@@ -0,0 +1,16 @@
+###############################################
+#
+# pip install pynput
+#
+###############################################
+
+from pynput.mouse import Controller,Button
+import time
+
+mouse = Controller()
+
+while True:
+    mouse.click(Button.left,1)
+    print('clicked')
+
+    time.sleep(5)
